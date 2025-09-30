ae33f135f5023eea33b57fa6db712826a6f2616f
Zack <zacharycollier36@protonmail.com>
2025-09-30 14:01:27 -0230

    Fixed head import and removed extra nav h1

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,6 @@ import NavButton from "./NavButton";
 export default function Nav() {
   return (
     <nav>
-      <h1>Zachary Collier</h1>
       <NavButton name='Home' route='/' />
       <NavButton name='Contact' route='/contact' />
     </nav>

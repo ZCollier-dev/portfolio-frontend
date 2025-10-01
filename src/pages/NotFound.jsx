997702b12274859f19d85a3eae381be8d8d82b42
Zack <zacharycollier36@protonmail.com>
2025-10-01 14:47:08 -0230

    Added the initial CSS for the site

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <main>
       <div class='notfound'>
-        <h2>404: Not Found</h2>
+        <h2>404: Page Not Found</h2>
       </div>
     </main>
   );
